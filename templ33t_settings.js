cbbf91d2ec4b3e2d2a2e04420fc98dd9931cf051
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 08:20:34 +0000

    removed unused js
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_db_config@295121 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templ33t_settings.js
+++ templ33t_settings.js
@@ -19,15 +19,3 @@ jQuery(document).ready(
 
 	}
 );
-
-function templ33t_configTheme() {
-
-	jQuery('div.templ33t_blocks div.templ33t_active').removeClass('templ33t_active').addClass('templ33t_hidden');
-	jQuery('div.templ33t_blocks div.templ33t_block_list_'+jQuery(this).attr('rel')).removeClass('templ33t_hidden').addClass('templ33t_active');
-
-	jQuery('div.templ33t_themes li.selected').removeClass('selected');
-	jQuery(this).addClass('selected');
-	
-	return false;
-
-}
