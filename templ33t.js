05701fb66a7c70c5fa2237b2d7785651cb93e67c
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-26 09:03:20 +0000

    Fixing tab spacing
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_db_config@415172 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templ33t.js
+++ templ33t.js
@@ -130,7 +130,7 @@ function templ33t_placeControl() {
 	// calculate prev/next
 	var w = 0;
 	jQuery('li', nl).each(function() { w += jQuery(this).outerWidth()+parseInt(jQuery(this).css('margin-right')); });
-	if((w+40) > nl.width()) {
+	if(w > nl.width()) {
 		tc.prepend('<span style="display: block; width: 30px; height: 30px; float: left;">&nbsp;</span>');
 		nl.css('margin-left', '30px');
 		nl.css('margin-right', '30px');
