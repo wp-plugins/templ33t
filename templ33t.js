215cbd710f296bd323d58da6e660bfc9d4651f3d
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-27 01:02:38 +0000

    template name issue
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_db_config@415713 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templ33t.js
+++ templ33t.js
@@ -260,8 +260,6 @@ function templ33t_switchMode(switchTo) {
 					
 					var tslg = jQuery('select#page_template').val() == 'default' ? TL33T_current.default_template : jQuery('select#page_template').val();
 					
-					alert(tslg+' - '+(tslg in TL33T_def));
-					
 					if(!TL33T_def[tslg].blocks[(jQuery(this).index() - 1)].custom) {
 
 						cval = jQuery('#templ33t_val_'+crel).val();
