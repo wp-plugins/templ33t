aedaad6fd530644442e0b741d9cf9fed243336a8
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-27 20:38:14 +0000

    troubleshooting templ33t mode issues
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_db_config@416084 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templ33t_object.php
+++ templ33t_object.php
@@ -530,18 +530,10 @@ class Templ33t {
 		
 		if(!$this->active || !empty($this->meta)) return;
 		
-		// refresh once if missing post id
-		//if(empty($post->id) && !array_key_exists('tl33tredir', $_GET)) {
-		//	wp_redirect(add_query_arg('tl33tredir', '1', $_SERVER['REQUEST_URI']));
-		//	exit();
-		//}
-		
 		// cleanse default page name
 		if(empty($post->page_template) || $post->page_template == 'default') {
 			$post->page_template = $this->default_template = basename(get_page_template());
 		}
-		
-		print_r($post);
 		
 		if(array_key_exists($post->page_template, $this->templates)) {
 
@@ -879,6 +871,12 @@ class Templ33t {
 	function tabElements() {
 
 		global $templ33t, $templ33t_templates, $templ33t_options, $templ33t_meta, $templ33t_render, $post;
+		
+		// refresh once if missing post id
+		if(empty($post->id) && !array_key_exists('tl33tredir', $_GET)) {
+			wp_redirect(add_query_arg('tl33tredir', '1', $_SERVER['REQUEST_URI']));
+			exit();
+		}
 		
 		// keep track of selected template
 		if(empty($post->page_template) || $post->page_template == 'default') {
