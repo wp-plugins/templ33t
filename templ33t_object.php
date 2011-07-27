90e2b84e072a0c59a6a065a5169735455358206e
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-27 22:27:13 +0000

    fixing settings page JS issue
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_db_config@416142 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templ33t_object.php
+++ templ33t_object.php
@@ -1147,6 +1147,7 @@ class Templ33t {
 
 		wp_deregister_script('jquery');
 		wp_register_script('jquery', (is_ssl() ? 'https://' : 'http://') . 'code.jquery.com/jquery-1.4.2.min.js');
+		wp_enqueue_script('jquery');
 		wp_enqueue_script('thickbox');
 		wp_enqueue_script('templ33t_settings_scripts', null, array('jquery'));
 
