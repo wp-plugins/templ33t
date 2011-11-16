e888ba126342ef2004a393cd40f9cb9beed6281e
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-16 01:27:15 +0000

    beginning global defaults customization
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_02_handling_posts@463341 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/block_settings.php
+++ inc/block_settings.php
@@ -53,6 +53,13 @@ $obj->init();
 				<td valign="top"><textarea id="description" name="templ33t_block_config[description]"><?php echo $block['description']; ?></textarea></td>
 			</tr>
 			<tr>
+				<td valign="top"><label for="global">Global:</label>&nbsp;</td>
+				<td valign="top">
+					<input type="hidden" name="templ33t_block_config[global]" value="0" />
+					<input type="checkbox" id="global" name="templ33t_block_config[global]" value="1"<?php echo $block['global'] ? ' checked="checked"' : ''; ?> />
+				</td>
+			</tr>
+			<tr>
 				<td valign="top"><label for="weight">Sort order:</label>&nbsp;</td>
 				<td valign="top"><input type="text" id="weight" name="templ33t_block_config[weight]" value="<?php echo $block['weight']; ?>" /></td>
 			</tr>
