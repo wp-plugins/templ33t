e77d15849ca57244bc6079183cb086913c1c88e8
technosis <technosis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 08:31:17 +0000

    adding stripslashes to text values
    git-svn-id: https://plugins.svn.wordpress.org/templ33t/branches/01_02_handling_posts@562252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugs/text/templ33t_text.php
+++ plugs/text/templ33t_text.php
@@ -22,7 +22,7 @@ class Templ33tText extends Templ33tPlugin implements Templ33tTab, Templ33tOption
 		$str .= '<input type="hidden" name="templ33t_block_config[multiline]" value="0" />';
 		$str .= '<input type="checkbox" id="multiline" name="templ33t_block_config[multiline]" value="1" />';
 		$str .= '</td></tr><tr><td><label for="default">Default Value</label></td><td>';
-		$str .= $this->multiline ? '<textarea name="templ33t_block_config[default]">'.$this->default.'</textarea>' : '<input type="text" name="templ33t_block_config[default]" value="'.$this->default.'" size="60" />';
+		$str .= $this->multiline ? '<textarea name="templ33t_block_config[default]">'.$this->default.'</textarea>' : '<input type="text" name="templ33t_block_config[default]" value="'.stripslashes($this->default).'" size="60" />';
 		$str .= '</td></tr>';
 		
 		return $str;
@@ -37,7 +37,7 @@ class Templ33tText extends Templ33tPlugin implements Templ33tTab, Templ33tOption
 		
 		$str .= '<label>Text:</label> <br/> ';
 		
-		$str .= $this->multiline ? '<textarea name="meta['.$this->id.'][value]">'.$this->value.'</textarea>' : '<input type="text" name="meta['.$this->id.'][value]" value="'.$this->value.'" size="60" />';
+		$str .= $this->multiline ? '<textarea name="meta['.$this->id.'][value]">'.stripslashes($this->value).'</textarea>' : '<input type="text" name="meta['.$this->id.'][value]" value="'.stripslashes($this->value).'" size="60" />';
 		
 		$str .= '</div></div>';
 		
@@ -50,16 +50,16 @@ class Templ33tText extends Templ33tPlugin implements Templ33tTab, Templ33tOption
 		$val = empty($this->value) ? $this->default : $this->value;
 		
 		if(!$ret) {
-			echo $this->multiline ? nl2br($val) : $val;
+			echo $this->multiline ? stripslashes(nl2br($val)) : stripslashes($val);
 		} else {
-			return $this->multiline ? nl2br($val) : $val;
+			return $this->multiline ? stripslashes(nl2br($val)) : stripslashes($val);
 		}
 
 	}
 
 	function displayOption() {
 
-		$str = '<tr><td>'.$this->label.'</td><td><input type="hidden" name="meta['.$this->id.'][key]" value="templ33t_option_'.$this->slug.'" /><input type="text" name="meta['.$this->id.'][value]" value="'.$this->value.'" /></td></tr>';
+		$str = '<tr><td>'.$this->label.'</td><td><input type="hidden" name="meta['.$this->id.'][key]" value="templ33t_option_'.$this->slug.'" /><input type="text" name="meta['.$this->id.'][value]" value="'.stripslashes($this->value).'" /></td></tr>';
 
 		return $str;
 
